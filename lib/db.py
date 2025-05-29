f6f5934d551e4725a693f59241ef31acec04c1b6
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-05-29 14:20:28 +0300

    table

--- lib/db.py
+++ lib/db.py
@@ -3,3 +3,16 @@ DB_NAME = "workout.db"
 
 def get_connection():
     return sqlite3.connect(DB_NAME)
+def init_db():
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS workouts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                date TEXT NOT NULL,
+                type TEXT NOT NULL,
+                duration INTEGER NOT NULL,
+                notes TEXT
+            )
+        """)
+        conn.commit()
