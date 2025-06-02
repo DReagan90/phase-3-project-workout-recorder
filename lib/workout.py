f0b9dec599f81cf09b9717391ff030313b4584b4
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-06-02 22:36:13 +0300

    view by date

--- lib/workout.py
+++ lib/workout.py
@@ -1,5 +1,6 @@
 from db import insert_workout
 from datetime import datetime
+from db import fetch_workouts_by_date
 
 def add_workout():
     date = input("Enter date (YYYY-MM-DD) or press Enter for today: ")
@@ -12,3 +13,14 @@ def add_workout():
 
     insert_workout(date, workout_type, duration, notes)
     print("✅ Workout added!")
+
+def view_workouts_by_date():
+    date = input("Enter date (YYYY-MM-DD): ")
+    workouts = fetch_workouts_by_date(date)
+
+    if not workouts:
+        print(f"No workouts recorded on {date}.")
+    else:
+        print(f"🏋️ Workouts on {date}:")
+        for i, (type_, duration, notes) in enumerate(workouts, 1):
+            print(f"{i}. {type_} - {duration} minutes. Notes: {notes}")
